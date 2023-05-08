824c9294a610be784998fa5c841c7d4159c02de4
Gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2023-05-08 23:31:48 +0300

    feat: add solution for the task #4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const folderPath = path.join(__dirname, 'files');
+const cloneFolderPath = path.join(__dirname, 'files-copy');
+
+fs.rm(cloneFolderPath, {force: true, recursive :true}, () => {
+  fs.mkdir(cloneFolderPath, { recursive: true }, () => {
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        console.log('Error');
+      } else {
+        files.forEach((item) => {
+          fs.copyFile(`${folderPath}\\${item}`, `${cloneFolderPath}\\${item}`, (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('File copied successfully');
+            }
+          });
+        });
+      }
+    });
+  });
+});
 
