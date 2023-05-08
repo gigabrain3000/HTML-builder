88bb421414abfbc453c5748c5c86d99ce447256e
Gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2023-05-08 21:41:01 +0300

    feat: add so;ution for task #3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const textPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(textPath, {withFileTypes: true}, (err, files) => {
+  if (err) {
+    console.log('Error');
+  } else {
+    files.forEach((item) => {
+      if (!item.isFile()) {
+        console.log(`Error! ${item.name} is not a file!`);
+      }
+      else {
+      const {name} = item;
+      let extension = path.extname(`${name}`);
+      fs.stat(`${textPath}\\${name}`, (err, states) => {
+        if (err) {
+          console.log(`Error!`);
+        }
+        else {
+          console.log(`${name.split('.')[0]} - ${extension.slice(1)} - ${Math.round(states.size / 1024)}kb`);
+        }
+      });
+      }
+    });
+  }
+});
